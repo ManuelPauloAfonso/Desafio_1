19598547ae8a9c2db5243e595feb5013d8a3a120
Manuel Afonso <manuelpaulafonso29@gmail.com>
2022-04-15 18:28:34 +0100

    initial

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,  ChangeEvent } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -17,7 +17,7 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     const idTask = Math.random()
-    if(newTaskTitle === ""){
+    if(newTaskTitle !== ""){
       setTasks([{
         id: idTask,
         title: newTaskTitle,
@@ -28,19 +28,28 @@ export function TaskList() {
       ])
 
     }else{
-      return
+      return;
     }
     setNewTaskTitle('')
-
-   
   }
+  
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updateTasks = tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete
+      }
+      return task;
+    })
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const findTask = tasks.filter((task)=> task.id !== id);
+    setTasks(findTask)
+
   }
 
   return (
